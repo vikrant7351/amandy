11a0230fea99f427ca5a42ef55dfe0ff579df61f
vikrant <vikrant9400@gmail.com>
2023-09-20 12:33:48 +0530

    lastestversion

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  
+  return (
+    <Router>
+    <Routes>
+      <Route></Route>
+    </Routes>
+  </Router>
+  );
+}
+
+export default App;
+
