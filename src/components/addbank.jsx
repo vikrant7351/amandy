d76004787ed19f5fb79a39f6da976c188a209157
vikrant <vikrant9400@gmail.com>
2023-10-03 17:47:56 +0530

    v1.0.2

---
+++ src/components/addbank.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+
+const BankDetailsForm = () => {
+  const [formdata, setformdata] = useState({
+    accountHolderName: '',
+    accountNumber: '',
+    bankName: '',
+    IfscCode: '',
+    TransectionType: '',
+
+  })
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setformdata({
+      ...formdata,
+      [name]: value,
+    });
+  }
+
+
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+
+
+    try {
+      const response = Axios.post('http://localhost:4000/api/addBank', formdata, {
+        headers: {
+          'Content-type': 'application/json',
+        },
+      });
+
+      if(response.status === 200){
+        toast.success('add bank sucessfully');
+      } else {
+        console.error('bank registion failed');
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error('an error occured');
+    }
+  }
+
+
+
+
+
+  return (
+    <div className="page-containers">
+      <ToastContainer />
+      <div className="support-ticket-form">
+        <h2>Bank Details Form</h2>
+        <form onSubmit={handlesubmit} >
+          <div className="form-group">
+            <label htmlFor="accountHolderName">Account Holder Name:</label>
+            <input
+              type="text"
+              id="accountHolderName"
+              name="accountHolderName"
+              value={formdata.accountHolderName}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="accountNumber">Account Number:</label>
+            <input
+              type="text"
+              id="accountNumber"
+              name="accountNumber"
+              value={formdata.accountNumber}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="bankName">Bank Name:</label>
+            <input
+              type="text"
+              id="bankName"
+              name="bankName"
+              value={formdata.bankName}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="IfscCode">IFSC Code:</label>
+            <input
+              type="text"
+              id="IfscCode"
+              name="IfscCode"
+              value={formdata.IfscCode}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="TransectionType">TransectionType:</label>
+            <select
+              id="TransectionType"
+              name="TransectionType"
+              value={formdata.TransectionType}
+              onChange={handleInputChange}
+            >
+              <option value="Current">Current</option>
+              <option value="Saving">Saving</option>
+            </select>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default BankDetailsForm;
